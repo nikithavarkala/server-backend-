543512a06d6aaf148994189772248ffe19716a58
nikithavarkala <117817663+nikithavarkala@users.noreply.github.com>
2023-06-29 12:59:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const cors=require("cors");
 
 const app = express();
 
-app.use(cors({
-    origin:"*"
-}))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(express.json())
 
 // app.use(route);
